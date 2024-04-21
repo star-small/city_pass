c2593b63122dc2a7084ea48948ab87aa1211c543
ThinkPad Arch <starsmall890@gmail.com>
2024-04-21 13:53:48 +0500

    fix

--- bot.py
+++ bot.py
@@ -1,97 +1,91 @@
-#!/usr/bin/python
-import geotools
-import scrapper
+import telebot
 import os
 import json
 from telebot import types
-# This is a simple echo bot using the decorator mechanism.
-# It echoes any incoming text messages.
-
-import telebot
-
+import geotools
+import scrapper
 API_TOKEN = '7009172269:AAG-3Jb1CVeYaQ6ub9_dWi7i4RBtCe5xNqo'
-
 bot = telebot.TeleBot(API_TOKEN)
 
-# Handle '/start' and '/help'
-user_choose = 0
-with open("/home/troy/dev/gdc/data.json", 'r', encoding='utf-8') as file:
-    data = json.load(file)
-# print(data)
+# Load data from file
+with open("data.json", 'r', encoding='utf-8') as file:
+    all_data = json.load(file)
 
+categories = ["All", "Historical", "Architectural", "Natural", "Religious", "Museums", "Touristic", "Entertainment Centers", "Cultural"]
+data = all_data  # This will be the filtered dataset according to the chosen category
 
-@bot.message_handler(commands=['help', 'start'])
+# Dictionary to keep user states
+user_states = {}
+
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    button_geo = types.KeyboardButton(
-        text="Send location", request_location=True)
+    button_geo = types.KeyboardButton(text="Send location", request_location=True)
     keyboard.add(button_geo)
-    text = "Я ваш интерактивный гид, призванный сделать ваш визит в Астану незабываемым. Независимо от того, ищете ли вы индивидуальное путешествие по самым потрясающим достопримечательностям города или вам нужна информация о достопримечательностях Астаны, я здесь, чтобы помочь.\nВот что я могу для вас сделать: \n✅ Создавайте персонализированные маршруты в зависимости от вашего местоположения, предпочтений и вида транспорта.\n✅ Предоставьте подробную информацию о достопримечательностях, включая описания, исторический контекст, часы работы и информацию о билетах.\n✅ Помогайте с запросами, связанными с Citypass Astana, чтобы путешествие прошло без осложнений.\nДавайте исследуем Астану вместе.! 🚀\n/list /help"
-    bot.send_message(message.chat.id, text)
-@bot.message_handler(commands=['update'])
-def update(message):
-    bot.send_message(message.chat.id, "in process")
-    scrapper.dump_data()
-    bot.send_message(message.chat.id, "done")
+    welcome_text = ("Welcome to the Astana Interactive Guide. Please use the commands to navigate the bot.\n"
+                    "/categories to filter by category.\n"
+                    "/list to list attractions based on category.\n"
+                    "/help to see this message again.")
+    bot.send_message(message.chat.id, welcome_text, reply_markup=keyboard)
 
+@bot.message_handler(commands=['categories'])
+def category_list(message):
+    markup = types.ReplyKeyboardMarkup(row_width=1, one_time_keyboard=True, resize_keyboard=True)
+    for category in categories:
+        markup.add(types.KeyboardButton(text=category))
+    bot.send_message(message.chat.id, "Please select a category:", reply_markup=markup)
 
-@ bot.message_handler(commands=['list'])
-def send_list(message):
-    if os.path.exists("/home/troy/dev/gdc/data.json"):
-        text= ""
-        for i in range(len(data)):
-            text += str(i) + ". " + data[i]["name"] + "\n"
-        bot.send_message(message.chat.id, text)
+@bot.message_handler(func=lambda message: message.text in categories)
+def update_data_by_category(message):
+    global data
+    category = message.text
+    if category == "All":
+        data = all_data
     else:
-        scrapper.dump_data()
-        bot.send_message(message.chat.id, "Подождите....")
-        bot.send_message(message.chat.id, "Готово! Попробуйте ещё раз")
+        data = [item for item in all_data if category in item['categories']]
+    bot.send_message(message.chat.id, f"Category '{category}' selected. Use /list to see the attractions.")
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(commands=['list'])
+def send_list(message):
+    if data:
+        response_text = ""
+        for i, item in enumerate(data):
+            response_text += f"{i}. {item['name']}\n"
+        bot.send_message(message.chat.id, response_text)
+    else:
+        bot.send_message(message.chat.id, "No data available. Please select a category using /categories.")
 
-
-user_states= {}
-
-
-@ bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     try:
-        choose= int(message.text)
-        user_states[message.chat.id]= {'choose': choose}
-        print(choose)
-        item= data[choose]
-        name= item.get('name', 'Нет данных')
-        schedule= item.get('schedule', 'Расписание не доступно')
-        contact= item.get('contact', 'Контактная информация не предоставлена')
-        buses_list= " ".join(item.get('buses', [])) if item.get(
-            'buses') else 'Маршрутки не доступны'
+        choose = int(message.text)
+        user_states[message.chat.id] = {'choose': choose}
+        item = data[choose]
+        name = item.get('name', 'No data')
+        schedule = item.get('schedule', 'Schedule unavailable')
+        contact = item.get('contact', 'Contact information not provided')
+        buses_list = " ".join(item.get('buses', [])) if item.get('buses') else 'No bus routes available'
         geo = item["geo"]
-        response_text = f"{name}\nРасписание: {schedule}\nДоступные маршрутки: {buses_list}\nКонтакты: {contact}\nОтправьте свою геолокацию, чтоб я мог построить маршрут"
+        response_text = f"{name}\nSchedule: {schedule}\nAvailable bus routes: {buses_list}\nContacts: {contact}\nSend your location to build a route."
         bot.send_message(message.chat.id, response_text)
-
-        @bot.message_handler(content_types=['location'])
-        def handle_location(message):
-            choose = user_states[message.chat.id]['choose']
-            print(data[choose]["geo"], choose)
-            origin = f"{message.location.latitude}, {message.location.longitude}"
-            destination = f"{data[choose]['geo'][0]}, {data[choose]['geo'][1]}"
-            print(destination)
-            route_url = geotools.compute_route(
-                origin, destination)
-            bot.send_photo(message.chat.id, f"{route_url}")
-            route_message = f"[посмотреть](https://www.google.com/maps/dir/?api=1&origin={origin}&destination={destination}&travelmode=driving)"
-            bot.send_message(message.chat.id, route_message,
-                             parse_mode="markdown")
-
     except ValueError:
-        bot.send_message(
-            message.chat.id, "Пожалуйста, отправьте корректный номер.")
+        bot.send_message(message.chat.id, "Please send a correct index number.")
+    except IndexError:
+        bot.send_message(message.chat.id, "Index out of range, please try a valid index.")
     except TypeError:
-        bot.send_message(
-            message.chat.id, "Произошла ошибка типа данных, попробуйте еще раз.")
+        bot.send_message(message.chat.id, "Data type error occurred, please try again.")
 
+@bot.message_handler(content_types=['location'])
+def handle_location(message):
+    try:
+        choose = user_states[message.chat.id]['choose']
+        origin = f"{message.location.latitude}, {message.location.longitude}"
+        destination = f"{data[choose]['geo'][0]}, {data[choose]['geo'][1]}"
+        route_url =geotools.compute_route(origin, destination)
+        bot.send_photo(message.chat.id, f"{route_url}")  # Placeholder for sending the route map
+        route_message = f"View route: [Google Maps](https://www.google.com/maps/dir/?api=1&origin={origin}&destination={destination}&travelmode=driving)"
+        bot.send_message(message.chat.id, route_message, parse_mode="markdown")
+    except Exception as e:
+        bot.send_message(message.chat.id, "An error occurred while processing your location: " + str(e))
 
-bot.infinity_polling()
-bot.infinity_polling()
-bot.infinity_polling()
 bot.infinity_polling()
